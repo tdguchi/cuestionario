8984b0ac719261d63f3244c62080f853939d7689
tdguchi <tdguchi@gmail.com>
2025-02-08 01:16:04 +0100

    Recuperado una variable que pensé que no era necesaria

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from typing import List, Dict, Any, Optional
 from contextlib import contextmanager
 
 app = Flask(__name__)
+app.secret_key = "secret_key"
 DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1336425837788008508/HJgOTpQrnc03nKuBzU52ZzR2HX1-a0Hrtau0OjjoyPyTTeR6i7evyl9Xt2xU8pE9mUyn"
 
 @contextmanager
